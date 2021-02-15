413db28551d2a78a9c469648a9869be29e6ac26c
Sebastian Montagna <sebamontagna@gmail.com>
2021-02-15 15:50:39 -0300

    minor typo fix

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -14,7 +14,7 @@ export function Contact(props){
                 </div>
                 <div className="contact-right-column">
                     <h2 id="contact-title" className="arcade-font">{props.english ? "Thank you for passing by" : "Gracias por pasarte"}</h2>
-                    <p id="contact-text">{props.english ? "If you'd like to contact me, whether it is about a Job located Cordoba ARG (willing to relocate), a Remote Job, a Freelance Job, or just because you felt like it, you can find me in" : "Si te interesaría contactarme, ya sea para un trabajo Remoto, ubicado en Córdoba-ARG, Freelance, o por otra cuestión, me podés encontrar en"}</p>     
+                    <p id="contact-text">{props.english ? "If you'd like to contact me, whether it is about a Job located in Cordoba ARG (willing to relocate), a Remote Job, a Freelance Job, or just because you felt like it, you can find me in" : "Si te interesaría contactarme, ya sea para un trabajo Remoto, ubicado en Córdoba-ARG, Freelance, o por otra cuestión, me podés encontrar en"}</p>     
                     <div className="contact-options">
                         <a href="mailto:sebamontagna@gmail.com" target="_blank"><i className="far fa-envelope"></i></a>
                         <a href="https://github.com/sebamont" target="_blank"><i className="fab fa-github"></i></a>
