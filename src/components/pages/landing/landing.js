f4ba28491b34b5979e4dce5140aacbdf275a574a
Seba Mont <sebamontagna@gmail.com>
2021-02-24 12:09:14 -0300

    fixed some responsive issues, changed landing order

--- src/components/pages/landing/landing.js
+++ src/components/pages/landing/landing.js
@@ -38,13 +38,6 @@ export function Main(){
             </svg>
             </header>
             <section className="skills-container">
-                <div className="about-me">
-                    <h3 className="arcade-font">{english ? "About me" : "Sobre mi"}</h3>
-                    <p>{english ? "I was born in 1993 in Salta-Argentina, where I'm currently living. Went to college after school to get my Accountant degree in 2017. But after working a few years with taxes and other boring stuff, I've finally decided to take a step towards my dream to become a Developer." : "Nací en 1993 en Salta, Argentina, donde aún vivo. Fuí a la universidad despues del colegio para obtener mi título de Contador en el 2017. Pero después de varios años trabajando con impuestos y otras cosas aburridas, finalmente decidí dar el paso en dirección a mi sueño de convertirme en Desarrollador."}</p><br />
-                    <p>{english ? "In 2018 started my journey through multiple courses, tutorials, different programming languages. Jumping from Sofwared Dev, to Web Dev, to Game Dev, to Data Science and so on. I can't say I regret it, 'cause it helped me a lot to understand the fundamentals of programming and made me realize that Web Development was the final path for me. Well, and some Game Development but as a hobby." : "En el 2018 empezo mi expedición entre un montón de cursos, tutoriales, diferentes lenguajes de programación. Pase del Desarrollo de software de escritorio, a aplicaciones web, a desarrollo de juegos, a data science, y así. No puedo decir que me arrepienta porque me ayudó un montón a entender los fundamentos de la programación y me permitió darme cuenta de que el Desarrollo Web era el indicado para mi. Bueno, y un poco de desarrollo de Videojuegos, pero como hobby."}</p> <br />
-                    <p>{english ? "Then 2020 had arrived and everything changed. As many others, the pandemic kinda ruined my plans, so I was there, without an accounting job and with a lot of free time. 'Things happen for a reason'. I've heard that a lot, and I have finally understood it. Since then free-time, for me, became a synonym of an opportunity to study." : "Y el 2020 llegó y todo cambió. Como a muchos otros, la pandemia me arruinó los planes. Asi que ahí estuve, sin trabajo de Contador y con un monton de tiempo libre. \"Las cosas pasan por algo\". Escuché eso tantas veces, y ahora lo entiendo. Desde entonces, para mí, el tiempo libre se volvio una oportunidad de seguir estudiando."}</p><br />
-                    <p>{english ? "Python was love at first sight. So was Django for developing Web apps. But React appeared in my life and it was so amazing that let me no other choice than pick it for Front-end. Willing to learn new things tho." : "Python fue amor a primera vista. También lo fue Django para el desarrollo de Web Apps. Y después apareció React que me pareció tan bueno que no me quedo otra que adoptarlo para el Front-End. Igual estoy dispuesto a aprender otras cosas."}</p>
-                </div>
                 
                 <div className="skills">
                     <h3 className="arcade-font">{english ? "Skills" : "Habilidades"}</h3>
@@ -71,6 +64,13 @@ export function Main(){
                     <h3 className="arcade-font">{english ? "Projects" : "Proyectos"}</h3>
                     <h5 className="arcade-font"><Link to="/projects">Press Start Button <small>{english ? "(Click Here)" : "(Click Aqui)"}</small></Link></h5>
                 </div>
+                <div className="about-me">
+                    <h3 className="arcade-font">{english ? "About me" : "Sobre mi"}</h3>
+                    <p>{english ? "I was born in 1993 in Salta-Argentina, where I'm currently living. Went to college after school to get my Accountant degree in 2017. But after working a few years with taxes and other boring stuff, I've finally decided to take a step towards my dream to become a Developer." : "Nací en 1993 en Salta, Argentina, donde aún vivo. Fuí a la universidad despues del colegio para obtener mi título de Contador en el 2017. Pero después de varios años trabajando con impuestos y otras cosas aburridas, finalmente decidí dar el paso en dirección a mi sueño de convertirme en Desarrollador."}</p><br />
+                    <p>{english ? "In 2018 started my journey through multiple courses, tutorials, different programming languages. Jumping from Sofwared Dev, to Web Dev, to Game Dev, to Data Science and so on. I can't say I regret it, 'cause it helped me a lot to understand the fundamentals of programming and made me realize that Web Development was the final path for me. Well, and some Game Development but as a hobby." : "En el 2018 empezo mi expedición entre un montón de cursos, tutoriales, diferentes lenguajes de programación. Pase del Desarrollo de software de escritorio, a aplicaciones web, a desarrollo de juegos, a data science, y así. No puedo decir que me arrepienta porque me ayudó un montón a entender los fundamentos de la programación y me permitió darme cuenta de que el Desarrollo Web era el indicado para mi. Bueno, y un poco de desarrollo de Videojuegos, pero como hobby."}</p> <br />
+                    <p>{english ? "Then 2020 had arrived and everything changed. As many others, the pandemic kinda ruined my plans, so I was there, without an accounting job and with a lot of free time. 'Things happen for a reason'. I've heard that a lot, and I have finally understood it. Since then free-time, for me, became a synonym of an opportunity to study." : "Y el 2020 llegó y todo cambió. Como a muchos otros, la pandemia me arruinó los planes. Asi que ahí estuve, sin trabajo de Contador y con un monton de tiempo libre. \"Las cosas pasan por algo\". Escuché eso tantas veces, y ahora lo entiendo. Desde entonces, para mí, el tiempo libre se volvio una oportunidad de seguir estudiando."}</p><br />
+                    <p>{english ? "Python was love at first sight. So was Django for developing Web apps. But React appeared in my life and it was so amazing that let me no other choice than pick it for Front-end. Willing to learn new things tho." : "Python fue amor a primera vista. También lo fue Django para el desarrollo de Web Apps. Y después apareció React que me pareció tan bueno que no me quedo otra que adoptarlo para el Front-End. Igual estoy dispuesto a aprender otras cosas."}</p>
+                </div>
             </section>
             
         </motion.div>
@@ -78,12 +78,12 @@ export function Main(){
 }
 
 
-//position.y > 9000 ? props.divID will asign the id to the card so it will run the animation once we scrolled down 1000px (will show the animation perfectly on desktop)
+//position.y > 900 ? props.divID will asign the id to the card so it will run the animation once we scrolled down 1000px (will show the animation perfectly on desktop)
 //TODO: how to make that for mobile?
 function SkillCard(props){
     const {position} = useScrollData();
     return(
-        <div id={position.y>900? props.divId : ""}> 
+        <div id={position.y>300? props.divId : ""}> 
             <i className={props.iconClasses}></i> 
             <span>{props.skillName}</span>
             {props.children}
